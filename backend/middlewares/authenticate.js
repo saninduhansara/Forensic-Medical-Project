a320d286a010c0a6824163d4806e02b49e36aae9
saninduhansara <e22130@eng.pdn.ac.lk>
2026-06-24 00:27:57 +0530

    Create Authenticate middleware ☑️

---
+++ backend/middlewares/authenticate.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv"
+
+dotenv.config()
+
+export default function authenticate(req, res, next) {
+    const header = req.header("Authorization")
+
+    if (header == null) {
+        next()
+    } else {
+        const token = header.replace("Bearer ", "")
+
+        jwt.verify(token, process.env.JWT_SECRET_KEY,
+            (err, decoded) => {
+                if (decoded == null) {
+                    res.status(401).json({ message: "Invalid token please login again" })
+                    return
+                } else {
+                    req.user = decoded
+                    next()
+                }
+
+            }
+        )
+
+    }
+
+
+}
